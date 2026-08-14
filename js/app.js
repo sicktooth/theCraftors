3e9163263bcea8c20e0c64a04a487081569dd663
sicktooth <sicktooth003@gmail.com>
2026-08-14 21:34:39 +0100

    minified javascript

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-let mobileNavBtn = document.getElementById("mobileNavBtn"),
-    mainPage = document.getElementById("main-page"),
-    mobileNavPage = document.getElementById("mobileNavPage");
-
-const showMobileNav = () =>{
-    mobileNavPage.classList.remove('hidden');
-    mobileNavPage.classList.remove('slide-left')
-    mobileNavPage.classList.add('slide-right');
-    mainPage.classList.add('blur-xs');
-}
-const RmMobileNav = () => {
-    mobileNavPage.classList.remove('slide-right')
-    mobileNavPage.classList.add('slide-left');
-    setTimeout(() => {
-        mobileNavPage.classList.add('hidden');
-    }, 300);
-    
-    mainPage.classList.remove('blur-xs');
-}
+let mobileNavBtn = document.getElementById("mobileNavBtn"),mainPage = document.getElementById("main-page"),mobileNavPage = document.getElementById("mobileNavPage");
+const showMobileNav = () =>{mobileNavPage.classList.remove('hidden');mobileNavPage.classList.remove('slide-left');mobileNavPage.classList.add('slide-right');mainPage.classList.add('blur-xs');}
+const RmMobileNav = () => {mobileNavPage.classList.remove('slide-right');mobileNavPage.classList.add('slide-left'); setTimeout(() => {mobileNavPage.classList.add('hidden');}, 300);mainPage.classList.remove('blur-xs');}
